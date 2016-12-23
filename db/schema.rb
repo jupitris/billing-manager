7c05cb5a23542c21f8c0b273b5c55e09df5cbd72
Hiroyuki Maruyama <hiroyuki.maruyama@duxi.co.jp>
2016-12-23 18:43:53 +0900

    Replace model with devise and so on.
    Rollback users controller and user model.
    Create user by using devise.

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161219091954) do
-
-  create_table "users", force: :cascade do |t|
-    t.integer  "group"
-    t.string   "name"
-    t.string   "email"
-    t.string   "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
